5059ae013e89137b6fc62791882134f619c8a72f
Adacasey <159260686+Adacasey@users.noreply.github.com>
2025-11-26 23:10:29 +0000

    app js updated

--- App.js
+++ App.js
@@ -9,6 +9,28 @@ export default function App() {
     </View>
   );
 }
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeScreen from "./src/screens/HomeScreen";
+import ExpensesScreen from "./src/screens/ExpensesScreen";
+import ChallengesScreen from "./src/screens/ChallengesScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Expenses" component={ExpensesScreen} />
+        <Tab.Screen name="Challenges" component={ChallengesScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
